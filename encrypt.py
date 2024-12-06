6435e26d78e4786c4765eed338502bc40f4c6013
Tilley <tilleyy8@gmail.com>
2024-12-05 22:11:59 -0700

    attempt to fix x11

--- encrypt.py
+++ encrypt.py
@@ -1,13 +1,16 @@
-from Crypto.Cipher import AES # sudo pacman -S python-pycryptodome
+from Crypto.Cipher import AES  # sudo pacman -S python-pycryptodome
 from Crypto.Util.Padding import pad, unpad
 from hashlib import sha256
 import subprocess, base64, os, platform
+
 password = "amongus_sus_meow"
+
 def copy_to_clipboard(text):
-    if subprocess.check_output(["which", "wl-copy"]):
+    if os.environ.get("XDG_SESSION_TYPE") == "wayland" and subprocess.check_output(["which", "wl-copy"], stderr=subprocess.DEVNULL):
         subprocess.run(["wl-copy"], input=text.encode())
-    elif subprocess.check_output(["which", "xclip"]):
+    elif os.environ.get("XDG_SESSION_TYPE") == "x11" and subprocess.check_output(["which", "xclip"], stderr=subprocess.DEVNULL):
         subprocess.run(["xclip", "-selection", "clipboard"], input=text.encode())
+
 def encrypt(plaintext, passphrase):
     key = sha256(passphrase.encode()).digest()
     iv = os.urandom(AES.block_size)
@@ -15,6 +18,7 @@ def encrypt(plaintext, passphrase):
     padded_plaintext = pad(plaintext.encode(), AES.block_size)
     ciphertext = iv + cipher.encrypt(padded_plaintext)
     return base64.b64encode(ciphertext).decode()
+
 def decrypt(ciphertext, passphrase):
     key = sha256(passphrase.encode()).digest()
     ciphertext = base64.b64decode(ciphertext)
@@ -23,20 +27,26 @@ def decrypt(ciphertext, passphrase):
     cipher = AES.new(key, AES.MODE_CBC, iv)
     plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
     return plaintext.decode()
-if subprocess.check_output(["which", "wl-copy"]):
+
+if os.environ.get("XDG_SESSION_TYPE") == "wayland" and subprocess.check_output(["which", "wl-copy"], stderr=subprocess.DEVNULL):
     clipboard_content = subprocess.getoutput("wl-paste")
-elif subprocess.check_output(["which", "xclip"]):
+elif os.environ.get("XDG_SESSION_TYPE") == "x11" and subprocess.check_output(["which", "xclip"], stderr=subprocess.DEVNULL):
     clipboard_content = subprocess.getoutput("xclip -o -selection clipboard")
-if (clipboard_content[:2] == "&&"):
+else:
+    clipboard_content = ""
+
+if clipboard_content[:2] == "&&":
     input_text = clipboard_content
 else:
     input_text = input("Input Text: ")
+
 if input_text.startswith("&&"):
     decrypted_text = decrypt(input_text[2:], password)
     print("Decrypted text:", decrypted_text)
     copy_to_clipboard(decrypted_text)
 else:
     encrypted_text = encrypt(input_text, password)
-    encrypted_text = ("&&" + encrypted_text)
+    encrypted_text = "&&" + encrypted_text
     copy_to_clipboard(encrypted_text)
     print("Encrypted text copied")
+
